2afb79068446e74871dce7a9ac0310024ba8722f
jaixbhatia <66648216+jaixbhatia@users.noreply.github.com>
2023-11-23 16:02:34 -0800

    Update main.py

--- main.py
+++ main.py
@@ -9,13 +9,22 @@ def reverse_phone_lookup(yelp_fusion_api_key, gplaces_api_key, country_code, pho
     """
     phone_number_formatted = str(country_code) + str(phone_number)
     names =  gplaces.filter(gplaces_api_key, phone_number_formatted)
-    if not names: # empty list 
+    if not names: # nothing found from gplaces API 
         names = yelp_fusion.filter(yelp_fusion_api_key, phone_number_formatted)
-    return names
 
+    # caching step
+    if names:
+        if phone_number_formatted in consts.cache:
+            consts.cache[phone_number_formatted] += names
+        else:
+            consts.cache[phone_number_formatted] = names
+
+    return names
 
 # test
 user_inputted_country_code = input("Enter country code (ex. 1): ")
 user_inputted_number = input("Enter phone # (ex. 6503860386): ")
 lookup = reverse_phone_lookup(consts.FUSION_API_KEY, consts.GPLACES_API_KEY, user_inputted_country_code, user_inputted_number)
+print('*' * 50, '\n')
 print("Results: " + str(lookup))
+print("Cache: ", str(consts.cache))
