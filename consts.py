9a0d7f67dc2e4013c88d954ee7b0ad6cde651d90
jaixbhatia <jaixbhatia@gmail.com>
2023-11-24 00:31:23 -0800

    ✨ Caches to cache.json. Improved error handling

--- consts.py
+++ consts.py
@@ -1,4 +1,29 @@
+import json
+
 GPLACES_API_KEY = '' # ask jai for api key
 FUSION_API_KEY = '' # ask jai for api key
 
-cache = {}
+temp_cache = {}
+
+def save_dict_to_json(file_name, dictionary):
+    # Load existing data if file exists
+    try:
+        with open(file_name, 'r') as file:
+            existing_data = json.load(file)
+    except FileNotFoundError:
+        existing_data = {}
+
+    # Update existing data with new dictionary
+    existing_data.update(dictionary)
+
+    # Write updated data back to file
+    with open(file_name, 'w') as file:
+        json.dump(existing_data, file)
+
+def cache(names, phone_number_formatted):
+    if names:
+        if phone_number_formatted in temp_cache:
+            temp_cache[phone_number_formatted] += names
+        else:
+            temp_cache[phone_number_formatted] = names
+    save_dict_to_json('cache.json', temp_cache)
--- main.py
+++ main.py
@@ -1,24 +1,35 @@
-import requests
+import json
 import gplaces, yelp_fusion, consts
 
 def reverse_phone_lookup(yelp_fusion_api_key, gplaces_api_key, country_code, phone_number): 
     """
-    1) Check cache
-    2) Call each function of the pipeline. Cache when possible. Return business name
-    3) If business already with us, add business name
+    1) Format phone number (formatted_phone_number should have the form 1650380386)
+    2) Check cache
+    3) Call each function of the pipeline (with error checking)
+    4) Cache results
+    5) Return list of business names associated with the formatted_phone_number
     """
+    # 1)
     phone_number_formatted = str(country_code) + str(phone_number)
-    names =  gplaces.filter(gplaces_api_key, phone_number_formatted)
-    if not names: # nothing found from gplaces API 
-        names = yelp_fusion.filter(yelp_fusion_api_key, phone_number_formatted)
-
-    # caching step
-    if names:
-        if phone_number_formatted in consts.cache:
-            consts.cache[phone_number_formatted] += names
-        else:
-            consts.cache[phone_number_formatted] = names
-
+    # 2)
+    try:
+        with open('cache.json', 'r') as file:
+            cache_json = json.load(file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        cache_json = {}
+    if phone_number_formatted in cache_json:
+        return cache_json[phone_number_formatted]
+    # 3)
+    try:
+        names = gplaces.filter(gplaces_api_key, phone_number_formatted)
+        if not names: # if nothing found from gplaces API, run yelp fusion API
+            names = yelp_fusion.filter(yelp_fusion_api_key, phone_number_formatted)
+    except PermissionError as e:
+        print(f"Permission error occurred: {e}")
+        names = []
+    # 4)
+    consts.cache(names, phone_number_formatted)
+    # 5)
     return names
 
 # test
@@ -27,4 +38,4 @@ user_inputted_number = input("Enter phone # (ex. 6503860386): ")
 lookup = reverse_phone_lookup(consts.FUSION_API_KEY, consts.GPLACES_API_KEY, user_inputted_country_code, user_inputted_number)
 print('*' * 50, '\n')
 print("Results: " + str(lookup))
-print("Cache: ", str(consts.cache))
+print("Temp Cache: ", str(consts.temp_cache))
